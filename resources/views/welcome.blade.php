3c96bb23ff8211652753eaa2a2a223f53a19f6ce
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-02-16 15:10:24 -0300

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>somos devs</title>
+        <title>hello word</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
